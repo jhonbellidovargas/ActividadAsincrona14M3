fb648385474d825b11cb88e63b81dc5d24f50701
Jhon Bellido <jhonbellidovargas@gmail.com>
2022-12-20 19:08:21 -0500

    tarea14

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 let app = angular.module("MyApp", []);
 
 app.controller("formCtrl", function ($scope) {
-  $scope.auto = {kilometro : null, tarifa : null, monto:null, descuento: null, total : null};
-  $scope.calcular = function () {
-    let km = $scope.auto.kilometro;
-    let tarifa = 250;
-    let exceso = km - 300;
-    let monto = 0;
-    let descuento = 0;
-    let total = 0;
-    if (km > 300 && km <= 1000) {
-      monto = tarifa + exceso * 30;
-    } else if (km > 1000) {
-      monto = tarifa + 700 * 30 + (km - 1000) * 20;
-    } else {
-      monto = tarifa;
-    }
-    // Descuento
-    if (monto > 500) {
-      descuento = monto * 0.1;
-    } else {
-      descuento = 0;
-    }
-    total = monto - descuento;
-    $scope.auto.tarifa = tarifa;
-    $scope.auto.monto = monto;
-    $scope.auto.descuento = descuento;
-    $scope.auto.total = total;
+  $scope.empleado = {
+    codigo: 0,
+    horas: 0,
+    tarifa: 0,
+    salarioBruto: 0,
+    descuento: 0,
+    salarioNeto: 0,
   };
-  $scope.calcular();
+  $scope.calcularSalario = function () {
+    $scope.empleado.salarioBruto = $scope.empleado.horas * $scope.empleado.tarifa;
+    if ($scope.empleado.salarioBruto <= 500) {
+      $scope.empleado.descuento = 0;
+    } else if ($scope.empleado.salarioBruto > 500 && $scope.empleado.salarioBruto <= 1000) {
+      $scope.empleado.descuento = 0.02;
+    } else if ($scope.empleado.salarioBruto > 1000 && $scope.empleado.salarioBruto <= 4000) {
+      $scope.empleado.descuento = 0.08;
+    } else if ($scope.empleado.salarioBruto > 4000 && $scope.empleado.salarioBruto <= 8000) {
+      $scope.empleado.descuento = 0.15;
+    } else if ($scope.empleado.salarioBruto > 8000 && $scope.empleado.salarioBruto < 10000) {
+      $scope.empleado.descuento = 0.21;
+    } else if ($scope.empleado.salarioBruto >= 10000) {
+      $scope.empleado.descuento = 0.3;
+    }
+
+    $scope.empleado.salarioNeto = $scope.empleado.salarioBruto - ($scope.empleado.salarioBruto * $scope.empleado.descuento);
+  };
+  $scope.calcularSalario();
 });
